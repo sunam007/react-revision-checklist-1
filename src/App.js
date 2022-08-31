a4874aacc7a544cb54afdfbd90cecd1257147ec7
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-08-31 11:49:05 +0600

    checklist 1-8 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const headingStyle = {
+    color: "lightcoral",
+    textDecoration: "underline",
+    textAlign: "center",
+  };
+  const editorials = [
+    { heading: "Universal Pension", author: "Mir Ashraf Ali" },
+    { heading: "Question over EVM Security", author: "A K M Motin" },
+    { heading: "Foreign Trips", author: "P Bhattacharya" },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <article className="blog">
+        <h2 style={headingStyle}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </h2>
+        <p style={{ textAlign: "start", fontSize: "16px" }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab
+          aliquid repellendus ratione corrupti consequatur! Delectus voluptatem
+          non eius, necessitatibus laborum veritatis accusantium mollitia. Error
+          doloribus ab sint accusantium eligendi deserunt, ipsum ratione
+          repellendus sunt quos dolor commodi. Officiis dolore repellat ipsa
+          distinctio sed eos cumque obcaecati minima impedit voluptate!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </article>
+      {editorials.map((editorial) => (
+        <Blog heading={editorial.heading} author={editorial.author}></Blog>
+      ))}
+    </div>
+  );
+}
+
+function Blog(props) {
+  return (
+    <div className="blog">
+      <h3>"{props.heading}"</h3>
+      <h4 style={{ color: "lightcoral" }}>Author: {props.author}</h4>
     </div>
   );
 }
